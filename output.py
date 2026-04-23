69247d1f09bbc69d7897149cdf128af61e1e8a43
Arman <armanmomin78600@gmail.com>
2026-04-24 01:55:20 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,99 @@
+Python 3.12.10 (tags/v3.12.10:0cc8128, Apr  8 2025, 12:21:36) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+========== RESTART: C:/Users/IMAGE/Desktop/for vs code/archive/idk.py ==========
+
+ Missing Values:
+
+show_id            0
+type               0
+title              0
+director        2634
+cast             825
+country          831
+date_added        10
+release_year       0
+rating             4
+duration           3
+listed_in          0
+description        0
+dtype: int64
+
+ Data cleaned successfully
+
+
+==============================
+ Movies vs TV Shows
+==============================
+
+type
+Movie      5185
+TV Show     147
+Name: count, dtype: int64
+
+ Insight:
+Movies significantly outnumber TV Shows on Netflix.
+
+
+==============================
+Releases Over Time
+==============================
+
+release_year
+2012    163
+2013    197
+2014    242
+2015    349
+2016    577
+2017    657
+2018    648
+2019    519
+2020    442
+2021    161
+Name: count, dtype: int64
+
+ Insight:
+Content on Netflix increases significantly after 2010.
+
+
+==============================
+ Top Genres
+==============================
+
+listed_in
+International Movies        2369
+Dramas                      2293
+Comedies                    1553
+Action & Adventure           806
+Independent Movies           740
+Romantic Movies              579
+Thrillers                    547
+Children & Family Movies     503
+Documentaries                391
+Horror Movies                336
+Name: count, dtype: int64
+
+ Insight:
+Certain genres like Drama and International Movies dominate Netflix.
+
+
+==============================
+Top Countries Producing Content
+==============================
+
+country
+United States     2485
+India              940
+United Kingdom     484
+Canada             295
+France             293
+Germany            167
+Spain              161
+Japan              124
+China              109
+Mexico             101
+Name: count, dtype: int64
+
+ Insight:
+The United States produces the most Netflix content, followed by other countries.
+
